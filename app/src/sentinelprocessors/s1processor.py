5bcf4a66e5bbe0d1d5832952a65e98c7013c2c7d
Jacob Jeppesen <jhj@eng.au.dk>
2021-05-04 20:10:22 +0200

    Working version

--- app/src/sentinelprocessors/s1processor.py
+++ app/src/sentinelprocessors/s1processor.py
@@ -5,9 +5,10 @@ from pathlib import Path
 import numpy as np
 import rasterio
 import shutil
-from absl import flags, logging
+from absl import flags
 from matplotlib import pyplot as plt
 
+from loguru import logger
 from src.sentinelprocessors.base import BaseProcessor
 
 FLAGS = flags.FLAGS
@@ -27,7 +28,7 @@ class S1Processor(BaseProcessor):
         # https://stackoverflow.com/questions/31468176/setting-values-on-a-copy-of-a-slice-from-a-dataframe
         product = self.products_df.iloc[[index]].copy()
         try:
-            logging.info("Processing product: " + str(product['title'].values[0]))
+            logger.info("Processing product: " + str(product['title'].values[0]))
             self.unzip_product(product)
             product_path = product['product_path'].values[0]
 
@@ -46,13 +47,13 @@ class S1Processor(BaseProcessor):
             if self.del_intermediate:
                 shutil.rmtree(product_path / 'preprocessed')
 
-            logging.info("Finished processing product: " + str(product['title'].values[0]))
+            logger.info("Finished processing product: " + str(product['title'].values[0]))
         except:
-            logging.error("An error occured during proccesing of: " + str(product['title'].values[0]))
+            logger.error("An error occured during proccesing of: " + str(product['title'].values[0]))
 
     def preprocess(self, product_path, rel_orbit, pass_mode):
         graph_path = Path('data') / 'graphs' / 'preprocessGraph.xml'
-        specklefilter='None' # Use 'None' or 'Refined Lee' ('Refined Lee' might need "" or something around it to be parsed properly)
+        specklefilter = 'None'  # Use 'None' or 'Refined Lee' ('Refined Lee' might need "" or something around it to be parsed properly)
         dst_name = str(product_path.stem) + '_' + pass_mode + '_' + rel_orbit
         dst_path = (product_path / 'preprocessed' / dst_name).with_suffix('.dim')
         cmd = 'gpt {} -Pinput={} -Pspecklefilter={} -Poutput={}'.format(str(graph_path),
@@ -60,10 +61,11 @@ class S1Processor(BaseProcessor):
                                                                         str(specklefilter),
                                                                         str(dst_path))
         if not dst_path.exists() or self.overwrite_products:
+            logger.debug('Executing SNAP pre-processing graph for product: ' + str(product_path))
             self.exec_graph(cmd)
-            logging.debug('Sentinel-1 product has been preprocessed: ' + str(product_path))
+            logger.debug('Sentinel-1 product has been preprocessed: ' + str(product_path))
         else:
-            logging.debug('Sentinel-1 product has already been preprocessed: ' + str(product_path))
+            logger.debug('Sentinel-1 product has already been preprocessed: ' + str(product_path))
 
         return dst_path
 
@@ -81,10 +83,10 @@ class S1Processor(BaseProcessor):
 
         if not output_vh.exists() or self.overwrite_products:
             self.exec_graph(cmd)
-            logging.debug('Sentinel-1 product has had individual geotiff bands created: ' +
+            logger.debug('Sentinel-1 product has had individual geotiff bands created: ' +
                           str(preprocessed_product_path))
         else:
-            logging.debug('Sentinel-1 product already had individual geotiff bands created: ' +
+            logger.debug('Sentinel-1 product already had individual geotiff bands created: ' +
                           str(preprocessed_product_path))
 
         return [output_vh, output_vv, output_vv_vh]
@@ -117,18 +119,18 @@ class S1Processor(BaseProcessor):
 
             # Create cloud optimized geotiff
             # NOTE: Commented out for Elsevier paper to improve processing speed (the individual tiles are deleted after they have been combined anyways)
-#             self.create_cog(dst_path, compression='DEFLATE')
+            #             self.create_cog(dst_path, compression='DEFLATE')
 
             # Create thumbnail of the RGB image
             # NOTE: Commented out for Elsevier paper to improve processing speed (the individual tiles are deleted after they have been combined anyways)
-#             self.create_sentinel1_thumbnail(dst_path)
+            #             self.create_sentinel1_thumbnail(dst_path)
 
-            logging.debug('Sentinel-1 product has had RGB geotiff created: ' + str(dst_path))
+            logger.debug('Sentinel-1 product has had RGB geotiff created: ' + str(dst_path))
         else:
-            logging.debug('Sentinel-1 product already had RGB geotiff created: ' + str(dst_path))
+            logger.debug('Sentinel-1 product already had RGB geotiff created: ' + str(dst_path))
 
     def create_vrt_and_cog(self, product_date_abs_orbit, create_thumbnail=True):
-        logging.info("Creating Sentinel-1 vrt and geotiff file(s) for date and abs orbit: " + product_date_abs_orbit)
+        logger.info("Creating Sentinel-1 vrt and geotiff file(s) for date and abs orbit: " + product_date_abs_orbit)
         rgb_paths = []
         for index in range(len(self.products_df.index)):
             product = self.products_df.iloc[[index]].copy()
@@ -146,7 +148,7 @@ class S1Processor(BaseProcessor):
         vrt_name = satellite_name + '_' + product_date_abs_orbit + '_' + pass_mode + '_' + rel_orbit + '_RGB.vrt'
         vrt_path = Path(self.directory / 'output_data' / 's1' / 'combined' / vrt_name)
         self.create_vrt(src_paths=rgb_paths, vrt_path=vrt_path)
-        
+
         img_path = vrt_path.with_suffix('.tif')
         # File check has to be done here or COG will be made every time (there might be better way to implement this)
         if not img_path.exists() or self.overwrite_products:
@@ -155,7 +157,7 @@ class S1Processor(BaseProcessor):
             # Create cloud optimized geotiff
             self.vrt_to_geotiff(vrt_path, img_path, dst_format='COG', compression='DEFLATE')
             # The new vrt_to_geotiff() function should make cloud-optimized geotiff by default, so there should not be a reason to do it afterwards
-#             self.create_cog(img_path, compression='DEFLATE')
+            #             self.create_cog(img_path, compression='DEFLATE')
             if create_thumbnail:
                 self.create_sentinel1_thumbnail(img_path)
 
@@ -185,8 +187,8 @@ class S1Processor(BaseProcessor):
 
             # Move channels to last axis and normalize with the colours manually found (ie. vh=+30, vv=+20, vv-vh=+0)
             thumbnail = np.rollaxis(thumbnail, 0, 3)
-            thumbnail[:, :, 0] = np.clip((thumbnail[:, :, 0]+30) / 25, 0, 1)
-            thumbnail[:, :, 1] = np.clip((thumbnail[:, :, 1]+20) / 25, 0, 1)
+            thumbnail[:, :, 0] = np.clip((thumbnail[:, :, 0] + 30) / 25, 0, 1)
+            thumbnail[:, :, 1] = np.clip((thumbnail[:, :, 1] + 20) / 25, 0, 1)
             thumbnail[:, :, 2] = np.clip((thumbnail[:, :, 2]) / 25, 0, 1)
 
         plt.imsave(thumbnail_path, thumbnail)
@@ -195,7 +197,7 @@ class S1Processor(BaseProcessor):
     def exec_graph(cmd):
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in p.stdout.readlines():
-            logging.debug(line.strip().decode())
+            logger.debug(line.strip().decode())
         retval = p.wait()
 
     @staticmethod
@@ -206,12 +208,12 @@ class S1Processor(BaseProcessor):
                     # Find the text between the tags in the line (it is from an XML document)
                     index1 = [i for i, char in enumerate(line) if char == '>'][0]  # Index of first '>'
                     index2 = [i for i, char in enumerate(line) if char == '<'][1]  # Index of second '<'
-                    rel_orbit = line[index1+1:index2]
+                    rel_orbit = line[index1 + 1:index2]
                 if 's1:pass' in line:
                     # Find the text between the tags in the line (it is from an XML document)
                     index1 = [i for i, char in enumerate(line) if char == '>'][0]  # Index of first '>'
                     index2 = [i for i, char in enumerate(line) if char == '<'][1]  # Index of second '<'
-                    pass_mode = line[index1+1:index2]
+                    pass_mode = line[index1 + 1:index2]
 
         # Ensure that rel_orbit is a 3 character string
         rel_orbit = format(int(rel_orbit), '03d')
@@ -225,17 +227,16 @@ class S1Processor(BaseProcessor):
     def temp_coregister_function_for_elsevier_intepretability_paper(target_img, coreg_path):
         from arosics import COREG
         from pathlib import Path
-        import logging
 
         # Remember to set nodata value to 0 when saving the ortofoto
         reference_im_path = Path('/workspace/mnt/app/data/reference')
         im_reference = str(reference_im_path / 'ortofoto_reference_epsg4326.tif')
-#         im_target = '/workspace/mnt/app/data/output/output_data/s1/combined/S1A_20210324_037136_DSC_139_RGB.tif'
-#         dst_path = '/workspace/mnt/app/data/output/output_data/s1/combined/S1A_20210324_037136_DSC_139_RGB_coreg.tif'
+        #         im_target = '/workspace/mnt/app/data/output/output_data/s1/combined/S1A_20210324_037136_DSC_139_RGB.tif'
+        #         dst_path = '/workspace/mnt/app/data/output/output_data/s1/combined/S1A_20210324_037136_DSC_139_RGB_coreg.tif'
         im_target = str(target_img)
-        
+
         kwargs = {
-        #     'mask_baddata_ref': str(reference_im_path / 'ortofoto_reference_epsg4326_nodata_mask.tif'),
+            #     'mask_baddata_ref': str(reference_im_path / 'ortofoto_reference_epsg4326_nodata_mask.tif'),
             'path_out': str(coreg_path),
             'fmt_out': 'COG',
             'out_crea_options': ['COMPRESS=DEFLATE', 'NUM_THREADS=ALL_CPUS'],
@@ -250,4 +251,4 @@ class S1Processor(BaseProcessor):
         CR = COREG(im_reference, im_target, **kwargs)
         CR.calculate_spatial_shifts()
         CR.correct_shifts()
-        logging.info("Shift reliability is " + str(round(CR.shift_reliability, 2)) + "% for " + str(dst_path))
+        logger.info("Shift reliability is " + str(round(CR.shift_reliability, 2)) + "% for " + str(dst_path))
